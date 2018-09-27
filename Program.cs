9138c009eac8099ec80fa839d512c617d5f08ead
Calvin J Foster <cal.j.foster@gmail.com>
2018-09-26 20:59:50 -0500

    first push

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+
+namespace StringBuilderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          StringBuilder amountMsg = new StringBuilder("Your total amount is ");
+amountMsg.AppendFormat("{0:C} ", 60);
+
+Console.WriteLine(amountMsg);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
