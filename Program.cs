a752a32bac8483ec478665c42b3577965d3b4ccd
Calvin J Foster <cal.j.foster@gmail.com>
2018-09-27 21:05:17 -0500

    Second Push

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace StringBuilderApp
         {
           StringBuilder amountMsg = new StringBuilder("Your total amount is ");
 amountMsg.AppendFormat("{0:C} ", 60);
+        
+        StringBuilder amountMsg2 = new StringBuilder("Your total amount is ");
+amountMsg.AppendFormat("{0:C} ", 60);
 
 Console.WriteLine(amountMsg);
+Console.WriteLine(amountMsg2);
             Console.WriteLine("Hello World!");
         }
     }
